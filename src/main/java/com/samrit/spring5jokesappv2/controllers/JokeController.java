f3f66153737eee7449f01830e70be23a9cbeabb2
Samrit <hairaer02@gmail.com>
2021-11-26 13:19:42 +0300

    Adding Custom Banner

---
+++ src/main/java/com/samrit/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,20 @@
+package com.samrit.spring5jokesappv2.controllers;
+
+import com.samrit.spring5jokesappv2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJocks());
+        return "index";
+    }
+}
